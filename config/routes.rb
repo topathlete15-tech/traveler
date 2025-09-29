e3ca412ba8249db7ae5bd5da67d82bb00d13a499
topathlete15-tech <229550349+topathlete15-tech@users.noreply.github.com>
2025-09-30 06:12:09 +0900

    投稿機能を設定。令和７年９月３０日

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+  post 'posts' => 'posts#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,5 +9,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  
+  get 'top' => 'homes#top'
 end
