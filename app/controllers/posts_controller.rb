97865fe2cdd6eb17858a88ea16607f6d54134f07
topathlete15-tech <229550349+topathlete15-tech@users.noreply.github.com>
2025-10-22 05:21:09 +0900

    エラーの連続で全く進まず…。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,7 +34,7 @@ class PostsController < ApplicationController
   def destroy
     post = Post.find(params[:id])
     post.destroy
-    redirect_to post_path
+    redirect_to posts_path
   end
   
   private
