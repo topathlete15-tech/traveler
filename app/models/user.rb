479a701f2839ef85abb04f29a1a2ad312cba34b2
topathlete15-tech <229550349+topathlete15-tech@users.noreply.github.com>
2025-11-04 16:24:36 +0900

    第８節途中

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,23 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :follower, class_name: "Relationship", foregin_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foregin_key: "followed_id", dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed
+  has_many :follower_user, through: :followed, source: :follower
 
   attachment :profile_image
+
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  def unfollow
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    following_user.include?(user)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_03_092521) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_04_060441) do
   create_table "comments", force: :cascade do |t|
     t.text "comment"
     t.integer "user_id"
@@ -33,6 +33,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_03_092521) do
     t.datetime "updated_at", null: false
     t.string "image_id"
     t.integer "user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -62,4 +71,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_03_092521) do
     t.index ["reset_password_token"], name: "index_views_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "relationships", "users", column: "followed_id"
+  add_foreign_key "relationships", "users", column: "follower_id"
 end
